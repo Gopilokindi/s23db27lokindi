f1101124e1cf8bce3f573eb03620afe0b5ca774f
Gopilokindi <123197479+Gopilokindi@users.noreply.github.com>
2023-04-19 21:55:31 -0500

    updating all files

--- routes/gym.js
+++ routes/gym.js
@@ -4,6 +4,11 @@ var router = express.Router();
 const gym_controlers= require('../controllers/gym');
 /* GET costumes */
 router.get('/', gym_controlers.gym_view_all_Page );
-router.get('/gym/:id', gym_controlers.gym_detail)
+router.get('/gym/:id', gym_controlers.gym_detail);
+router.get('/detail', gym_controlers.gym_view_one_Page);
+router.get('/create', gym_controlers.gym_create_Page);
+router.get('/update', gym_controlers.gym_update_Page);
+router.get('/delete', gym_controlers.gym_delete_Page);
+
 module.exports = router;
 
